96405deabb04b19e9fd5afc32a4fde4e8be23434
root <root@master1.toronto.internal>
2018-10-04 01:36:51 +0000

    update script

---
+++ 1.deploy-applications.sh
@@ -0,0 +1,35 @@
+#0 Demo project
+oc new-project chat --display-name="Toronto Convergency Event Test Project - Chat Program" 
+oc adm policy add-scc-to-user privileged -z default,deployer -n chat
+oc delete limitrange --all
+
+# Demo 1 - Base deployment
+
+## Redis
+oc apply -f <(istioctl kube-inject -f scripts/applications/deployment/redis.yaml )
+
+## Client
+oc apply -n chat -f <(istioctl kube-inject -f scripts/applications/deployment/chat-client-v1.yaml)
+oc apply -n chat -f <(istioctl kube-inject -f scripts/applications/deployment/chat-client-android.yaml)
+oc apply -n chat -f <(istioctl kube-inject -f scripts/applications/deployment/chat-client-apple.yaml)
+
+## Server
+oc apply -n chat -f <(istioctl kube-inject -f scripts/applications/deployment/chat-server-v1.yaml)
+
+## Auth
+oc apply -n chat -f <(istioctl kube-inject -f scripts/applications/deployment/auth.yaml)
+
+## Ingress
+oc expose  svc istio-ingressgateway --name chat-client-ingress --hostname="chat-client.apps.toronto.openshiftworkshop.com" -n istio-system
+oc expose  svc istio-ingressgateway --name chat-server-ingress --hostname="chat-server-example-chat.apps.toronto.openshiftworkshop.com" -n istio-system
+
+## istio 
+### Access chat-client v1 only
+oc apply -n chat -f scripts/istio/1-0.chat-gateway.yaml 
+oc apply -n chat -f scripts/istio/1-1.vs-client-server.yaml  
+oc apply -n chat -f scripts/istio/1-2.destinationRule-chat-client.yaml 
+
+
+clear
+
+oc get pod -w 
